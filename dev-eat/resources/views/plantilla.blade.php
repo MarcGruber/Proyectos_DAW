ce4c5bbaeb820964e9133faa6b31da60215e1b5a
MarcGruber <mgruberlopez@gmail.com>
2022-12-01 10:52:55 +0100

    fix menu

--- dev-eat/resources/views/plantilla.blade.php
+++ dev-eat/resources/views/plantilla.blade.php
@@ -19,7 +19,11 @@
   <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  @if(strcmp(auth()->user()->id ,"cliente"))
+  <a class="navbar-brand" href="{{ url('/client') }}">DEV-EAT</a>
+  @else 
   <a class="navbar-brand" href="{{ url('/') }}">DEV-EAT</a>
+  @endif
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
